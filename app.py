723f24c92e088421fafe37a0a999a7659741204c
Harshith8801 <yharshith03@gmail.com>
2024-11-24 23:26:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -112,5 +112,6 @@ if __name__=="__main__":
     create_table()
     create_con()
     create_tables()
-    app.run(host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 5000))  # Use Render's port or default to 5000
+    app.run(host="0.0.0.0", port=port)
     
